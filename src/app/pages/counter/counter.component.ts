8bd847c2ff7c858ce365806246905b7d11afba5a
DrGus21 <oscartimoteo2020@gmail.com>
2025-09-25 16:05:43 -0500

    a

--- src/app/pages/counter/counter.component.ts
+++ src/app/pages/counter/counter.component.ts
@@ -1,3 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: ` <h1>Counter {{ counter }} </h1>
+<button (click)="counter = counter + 1">Aumentar</button>
+<button (click)="counter = counter - 1">Disminuir</button>
+<button (click)="counter = counter * 2">Duplicar</button>
+<button (click)="counter = counter / 2">Dividir</button>
+<button (click)="counter = 0">Reiniciar</button>
+`
+})
 export class CounterComponent {
-    
+    counter = 10;
 }

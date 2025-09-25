3a989043ef7cddb8bbbb04aceaeb94187812176c
DrGus21 <oscartimoteo2020@gmail.com>
2025-09-25 13:01:36 -0500

    equisde

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('clase');
+  protected readonly title = signal('wboncin');
 }
